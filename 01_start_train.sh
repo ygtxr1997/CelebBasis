0d8d5fd9a9b0c1ae2c0293cb11c5061f598ca9df
ygtxr1997 <gavingreenson@icloud.com>
2023-07-13 21:11:03 +0800

    add: colab support

--- 01_start_train.sh
+++ 01_start_train.sh
@@ -3,11 +3,12 @@
 export PYTHONPATH="${PWD}"
 
 SD_PATH=$1  # /Your/Path/To/sd-v1-4-full-ema.ckpt
+CONFIG_FILE=${2:-configs/stable-diffusion/aigc_id.yaml}
 
 # Usage Example:
 # ./start_train.sh ./weights/sd-v1-4-full-ema.ckpt
 
-python main_id_embed.py --base configs/stable-diffusion/aigc_id.yaml \
+python main_id_embed.py --base "${CONFIG_FILE}" \
                -t \
                --actual_resume "${SD_PATH}" \
                -n celebbasis \
